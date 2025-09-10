6f134a817f943c82638c535e122b578c69b84c91
oom2525181 <oom2525181@stu.o-hara.ac.jp>
2025-09-10 14:15:14 +0900

    test script

---
+++ Summon'sTower/Assets/New Folder/Scripts/TestScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    int test;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
