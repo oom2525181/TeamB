2535238bf0b9da1ea528017168953d993b829b2b
oom2525165 <oom2525165@stu.o-hara.ac.jp>
2025-09-12 17:45:46 +0900

    Button

--- Summon'sTower/Assets/CircleButton.cs
+++ Summon'sTower/Assets/CircleButton.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class CircleButton : MonoBehaviour
 {
+    public GameObject newPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            var pos = Camera.main.ScreenToWorldPoint(Input.mousePosition + Camera.main.transform.forward);
+            pos.z = -5;
+
+            GameObject newGameObject=Instantiate(newPrefab)as
+                GameObject;
+            newGameObject.transform.position = pos;
+        }
     }
+
 }
