d491a77b4ea79f90c0eb5630609f388c92d1207c
oom2525165 <oom2525165@stu.o-hara.ac.jp>
2025-09-17 10:40:56 +0900

    ちょっとのTagとGo Towerの変更

--- Summon'sTower/Assets/Scripts/GoTower.cs
+++ Summon'sTower/Assets/Scripts/GoTower.cs
@@ -6,7 +6,13 @@ public class GoTower : MonoBehaviour
 {
     private float speed = 5.0f;
     [SerializeField] Transform target;
-
+    private void Start()
+    {
+        if(CompareTag("Ally"))
+        target = GameObject.Find("Enemy'sTower").transform;
+        else if (CompareTag("Enemy"))
+            target = GameObject.Find("Ally'sTower").transform;
+    }
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
